3ecc42ea068501adb8f9b53b7d56b4101c85419f
Rosnel Carrasquel <120464649+Rosnel-Carrasquel@users.noreply.github.com>
2023-05-30 17:53:07 -0400

    Ajustes en prueba.py y index.html

--- prueba.py
+++ prueba.py
@@ -1,6 +1,8 @@
-mensaje = 'Este es un mensaje de para dejar claro que estoy en un curso de GitHub'
-print(mensaje)
 
-saludo = 'Hola a todos mis amigos'
-print(saludo)
+def potenciacion(base, exponente):
+    resultado = 0
+    for i in range(exponente):
+        resultado = resultado * base
+    return resultado
 
+    print(potenciacion(2, 3))
